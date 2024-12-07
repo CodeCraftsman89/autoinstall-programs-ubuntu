1895f01c27534c28797aa6895c50c87d612bfbb4
roman <appleid2505@yandex.ru>
2024-12-07 22:01:39 +0300

    edit snap.py

--- snap.py
+++ snap.py
@@ -1,12 +1,12 @@
 import subprocess
 
-def install_flathub_programs(flathub_programs):
-    for program in flathub_programs:
+def install_snap_programs(snap_programs):
+    for program in snap_programs:
         cmd = f"flatpak install flathub {program[0]}"
         subprocess.run(cmd, shell=True)
 
 
-def remove_flathub_programs(programs):
+def remove_snap_programs(programs):
     for program in programs:
         cmd = f"flatpak remove {program[0]}"
         subprocess.run(cmd, shell=True)
