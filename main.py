9091b7fbb888f74b0bca9b74ea8d9ca350b9db06
CodeCraftsman89 <appleid2505@yandex.ru>
2024-12-13 21:36:11 +0300

    Create pioneer station download and install script, add comments, add repositories apps

--- main.py
+++ main.py
@@ -16,28 +16,29 @@ all_flathub_programs = read_file('flathub.txt')
 all_snap_programs = read_file('snap.txt')
 
 
-while True:
-    print('1. Install programs')
-    print('2. Remove programs')
-    print('3. Install flathub programs')
-    print('4. Remove flathub programs')
-    print('5. Install snap programs')
-    print('6. Remove snap programs')
-    print('7. Exit')
-    choice = input('Enter your choice: ')
-    if choice == '1':
-        install_programs(all_programs)
-    elif choice == '2':
-        remove_programs(all_programs)
-    elif choice == '3':
-        install_flathub()
-    elif choice == '4':
-        remove_flathub_programs(all_flathub_programs)
-    elif choice == '5':
-        install_snap_programs(all_snap_programs)
-    elif choice == '6':
-        remove_snap_programs(all_snap_programs)
-    elif choice == '7':
-        break
-    else:
-        print('Invalid choice')
+if __name__ == "__main__":
+    while True:
+        print('1. Install programs')
+        print('2. Remove programs')
+        print('3. Install flathub programs')
+        print('4. Remove flathub programs')
+        print('5. Install snap programs')
+        print('6. Remove snap programs')
+        print('7. Exit')
+        choice = input('Enter your choice: ')
+        if choice == '1':
+            install_programs(all_programs)
+        elif choice == '2':
+            remove_programs(all_programs)
+        elif choice == '3':
+            install_flathub()
+        elif choice == '4':
+            remove_flathub_programs(all_flathub_programs)
+        elif choice == '5':
+            install_snap_programs(all_snap_programs)
+        elif choice == '6':
+            remove_snap_programs(all_snap_programs)
+        elif choice == '7':
+            break
+        else:
+            print('Invalid choice')
--- pioneer_station_run.py
+++ pioneer_station_run.py
@@ -1,6 +1,20 @@
-import os, subprocess
+"""
+This script changes the current working directory to the PioneerStation directory and runs the 'run.sh' script.
+
+The script uses the os and subprocess modules to change the current working directory and run the shell script.
+
+The 'pioneer_station_install' module is expected to provide the 'pioneer_dir' variable which is the path to the PioneerStation directory.
+
+Please ensure that the 'run.sh' script is executable and that the necessary permissions are set for the user running this script.
+"""
+
+import os
+import subprocess
 
 from pioneer_station_install import pioneer_dir
 
+# Change the current working directory to the PioneerStation directory
 os.chdir(pioneer_dir)
+
+# Run the shell script 'run.sh'
 subprocess.run("./run.sh")
