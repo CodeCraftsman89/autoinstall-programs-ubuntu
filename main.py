2bb03b81ad2649bca50047d4ede6f52a0f0b55fc
CodeCraftsman89 <appleid2505@yandex.ru>
2025-01-07 19:09:26 +0300

    Finish GUI

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ubuntu import install_programs, remove_programs
 from  snap import install_snap_programs, remove_snap_programs
 import tkinter as tk
 from tkinter import *
-from tkinter import messagebox
+from tkinter import ttk
 
 def read_file(filename):
     with open(filename, 'r') as file:
@@ -21,7 +21,7 @@ all_snap_programs = read_file(files[2])
 
 window = Tk()
 window.title ('Авноустановщик программ для Ubuntu')
-window.geometry("500x500")
+window.geometry("300x300")
 window.resizable(False, False)
 
 label = Label(text='Выберете нужную опцию')
@@ -53,5 +53,42 @@ label.pack()
             break
         else:
             print('Invalid choice')'''
+
+def btn_repo_click():
+    install_programs(all_programs)
+
+def btn_repo_r_click():
+    remove_programs(all_programs)
+
+def btn_flathub_click():
+    install_flathub()
+
+def btn_flathub_i_click():
+    install_flathub_programs(all_flathub_programs)
+
+def btn_flathub_r_click():
+    remove_flathub_programs(all_flathub_programs)
+
+def btn_snap_click():
+    install_snap_programs(all_snap_programs)
+
+def btn_snap_r_click():
+    remove_snap_programs(all_snap_programs)
+
+btn_repo = ttk.Button(window, text='Установить программы', command=btn_repo_click)
+btn_repo_r = ttk.Button(window, text='Удалить программы', command=btn_repo_r_click)
+btn_flathub = ttk.Button(window, text='Установить flathub', command=btn_flathub_click)
+btn_flathub_i = ttk.Button(window, text='Удалить flathub программы', command=btn_flathub_i_click)
+btn_flathub_r = ttk.Button(window, text='Удалить flathub программы', command=btn_flathub_r_click)
+btn_snap = ttk.Button(window, text='Установить snap', command=btn_snap_click)
+btn_snap_r = ttk.Button(window, text='Удалить snap', command=btn_snap_r_click)
+
+btn_repo.pack(expand=True, fill=X)
+btn_repo_r.pack(expand=True, fill=X)
+btn_flathub.pack(expand=True, fill=X)
+btn_flathub_i.pack(expand=True, fill=X)
+btn_flathub_r.pack(expand=True, fill=X)
+btn_snap.pack(expand=True, fill=X)
+btn_snap_r.pack(expand=True, fill=X)
 
 window.mainloop()
